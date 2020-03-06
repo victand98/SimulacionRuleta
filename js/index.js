7237bbc963444b6386b7a1f4233ed16ab128d9c8
Víctor Andrés Rojas <victor.rojas@unl.edu.ec>
2020-03-06 12:40:13 -0500

    Criterios de Comparación

--- js/index.js
+++ js/index.js
@@ -1,26 +1,60 @@
 $().ready(function () {
     $("#form").submit(e => {
         e.preventDefault();
+        // Recogiendo Datos del formulario
         var juego1 = parseInt($('#juego1').val());
         var juego2 = parseInt($('#juego2').val());
         var juego3 = parseInt($('#juego3').val());
         var juego4 = parseInt($('#juego4').val());
+        var juego4_lista = $('#juego4_lista').val().map(x => parseInt(x));
         var repeticiones = parseInt($('#repeticiones').val());
 
+        // Juego a
         var resultado1 = obtenerResultado(repeticiones,
             () => juego1 === 1 ? apuestaRojoNegro(rojas) : apuestaRojoNegro(negras));
         console.log('\n1. Apuesta al Rojo o Negro\n', resultado1);
+        // Criterios de comparación
+        var crt1Res1 = gananciasEsperadas(resultado1);
+        console.log('crt1Res1', crt1Res1);
+        var crt2Res1 = proporcionGanar(resultado1);
+        console.log('crt2Res1', crt2Res1);
+        var crt3Res1 = tiempoEsperado(resultado1);
+        console.log('crt3Res1', crt3Res1);
 
+        // Juego b
         var resultado2 = obtenerResultado(repeticiones, () => apuestaUnNumero(juego2));
         console.log('2. Apuesta un Número\n', resultado2);
+        // Criterios de comparación
+        var crt1Res2 = gananciasEsperadas(resultado2);
+        console.log('crt1Res2', crt1Res2)
+        var crt2Res2 = proporcionGanar(resultado2);
+        console.log('crt2Res2', crt2Res2);
+        var crt3Res2 = tiempoEsperado(resultado2);
+        console.log('crt3Res2', crt3Res2);
 
+        // Juego c
         var resultado3 = obtenerResultado(repeticiones,
             () => juego3 === 1 ? apuestaMartingala(rojas) : apuestaMartingala(negras));
         console.log('3. Apuesta de Martingala\n', resultado3);
+        // Criterios de comparación
+        var crt1Res3 = gananciasEsperadas(resultado3);
+        console.log('crt1Res3', crt1Res3);
+        var crt2Res3 = proporcionGanar(resultado3);
+        console.log('crt2Res3', crt2Res3);
+        var crt3Res3 = tiempoEsperado(resultado3);
+        console.log('crt3Res3', crt3Res3);
 
+        // Juego d
         var resultado4 = obtenerResultado(repeticiones,
-            () => juego4 === 1 ? apuestaLabouchere(rojas) : apuestaLabouchere(negras));
+            () => juego4 === 1 ? apuestaLabouchere(rojas, juego4_lista) : apuestaLabouchere(negras, juego4_lista));
         console.log('4. Apuesta de Labouchere\n', resultado4);
+        // Criterios de comparación
+        var crt1Res4 = gananciasEsperadas(resultado4);
+        console.log('crt1Res4', crt1Res4);
+        var crt2Res4 = proporcionGanar(resultado4);
+        console.log('crt2Res4', crt2Res4);
+        var crt3Res4 = tiempoEsperado(resultado4);
+        console.log('crt3Res4', crt3Res4);
     });
 });
 
@@ -77,7 +111,7 @@ function apuestaMartingala(conjunto) {
 }
 
 // 4. Apuesta mediante el sistema de Labouchere
-function apuestaLabouchere(conjunto) {
+function apuestaLabouchere(conjunto, lista) {
     // Código aquí
 
     return { ganancia: 0, nroApuestas: 0 }
@@ -94,4 +128,33 @@ function obtenerResultado(repeticiones, func) {
     for (let i = 0; i < repeticiones; i++)
         result.push(func());
     return result;
+}
+
+/*
+    Criterios de comparación
+*/
+
+// Las ganancias esperadas por juego
+function gananciasEsperadas(resultado) {
+    return average(resultado.map(x => x.ganancia > 0 ? x.ganancia : 0));
+}
+
+// La proporción de juegos que ganas
+function proporcionGanar(resultado) {
+    return resultado.filter(x => x.ganancia > 0).length;
+}
+
+// El tiempo de juego esperado por juego
+function tiempoEsperado(resultado) {
+    return average(resultado.map(x => x.nroApuestas));
+}
+
+// La cantidad máxima que puede perder
+function maxPerdida(resultado) {
+
+}
+
+// La cantidad máxima que puedes ganar
+function maxGanancia(resultado) {
+
 }
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,5 @@
-function getRandomResult(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// Funcion para obtener un resultado randómico en el intervalo [min, max]
+const getRandomResult = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// Funcion para obtener el promedio en un arreglo de números
+const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
